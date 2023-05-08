cb1fad5f13b77bf71b94b430583ba8ee368eb685
Christian Ferreyra <chrisferreyra13@gmail.com>
2023-05-08 19:58:48 +0200

    started with te exploration, added bivariate data generators

--- itpg/data/__init__.py
+++ itpg/data/__init__.py
@@ -1,4 +1,9 @@
 """Real data and simulations for testing functions."""
 
 
-from .sims import generate_data, set_lin_relationship
+from .sim_univariate import generate_data, set_lin_relationship
+from .sim_multivariate import (generate_data_biv, generate_biv_normal,
+                               generate_circle, generate_clusters,
+                               generate_mirror, generate_mustache,
+                               generate_rotated_plane, generate_smile)
+from .sim_multivariate import (BIV_REL)
